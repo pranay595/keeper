f3de7c8b970609b81d2c40231dea7b7c7f1e85f8
Pranay Lohe <57897846+pranay595@users.noreply.github.com>
2025-05-23 14:38:27 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import CreateArea from './components/CreateArea';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Notes from './components/Notes';
+import "./styles.css"
+
+function App() {
+  const[notes,setNotes] = useState([]);
+
+  function addNote(note){
+    if(note.content.length>0){
+    setNotes((prevVal)=>{
+      return([...prevVal,note]);
+    });}
+  }
+
+  function deleteNote(id) {
+    setNotes((prevVal) => {
+      return (prevVal.filter((val,i)=>{
+        return i!==id;
+      }));
+    });
+  }
+
+  function updateNotes(id,note){
+    setNotes((prevVal)=>{
+      prevVal[id]=note;
+     return [...prevVal];
+    })
+  }
+
+  return (
+    <>
+      <Header/>
+      <CreateArea addNote={addNote}/>
+      {
+        notes.map((note, idx) => {
+          return <Notes key={idx}
+            id={idx}
+            title={note.title}
+            content={note.content}
+            deleteNote={deleteNote}
+            updateNotes={updateNotes} />
+        })
+      }
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
